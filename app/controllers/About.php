4a7bf87ab81428ba95e2fcc4b4c70b382088ee45
lukmantresnahadi <60495843+lukmantresnahadi@users.noreply.github.com>
2021-10-01 18:57:52 +0700

    menyelesaikan video controller

---
+++ app/controllers/About.php
@@ -0,0 +1,13 @@
+<?php 
+
+  class about {
+
+    public function index($nama ='Lukman Tresnahadi', $pekerjaan ='UI/UX designer'){
+      echo "Halo, nama saya $nama,saya adalah seorang $pekerjaan ";
+    }
+    public function page(){
+      echo 'About/page';
+    }
+  }
+
+?>
